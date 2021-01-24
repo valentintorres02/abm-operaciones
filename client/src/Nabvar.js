dbb10cdd1293a1803b59810377315e0c58015eee
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-01-24 19:51:59 -0300

    Update Nabvar.js
    New optimized and more styled navbar

--- client/src/Nabvar.js
+++ client/src/Nabvar.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Nabvar() {
   return (
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/abm">ABM</Link></li>
-    </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">
+              Home
+              <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="abm">ABM</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
   );
 }
 
