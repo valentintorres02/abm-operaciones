9e5a24ac9f6c6f8974c33a23b4fd7cce2524c3c8
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-01-24 19:51:14 -0300

    Optimize create operation section
    Created new CreateOperation component

--- client/src/Abm.js
+++ client/src/Abm.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Axios from 'axios';
-import dateFormat from 'dateformat';
 import List from './List';
-import UpdateList from './UpdateList';
+import CreateOperation from './CreateOperation';
 
-function Abm({ operationsList, setOperationsList }) {
+function Abm({ fetchData, operationsList, setOperationsList }) {
   const [operationConcept, setOperationConcept] = useState('');
   const [operationAmount, setOperationAmount] = useState('');
   const [operationDate, setOperationDate] = useState('');
   const [operationType, setOperationType] = useState('in');
-  const [updateClass, setUpdateClass] = useState('hidden');
-  // const [newAmount, setNewAmount] = useState('');
-  const [operationToUpdate, setOperationToUpdate] = useState({});
+
+  const [newConcept, setNewConcept] = useState('');
+  const [newAmount, setNewAmount] = useState(0);
+  const [newDate, setNewDate] = useState('');
 
   const submitOperation = () => {
     Axios.post('http://localhost:3001/api/insert', {
@@ -22,129 +22,64 @@ function Abm({ operationsList, setOperationsList }) {
     setOperationsList([...operationsList, {
       operationConcept, operationAmount, operationDate, operationType,
     }]);
+    console.log('Operation submitted');
   };
 
   const deleteOperation = (operation) => {
     Axios.delete(`http://localhost:3001/api/delete/${operation}`);
   };
 
-  // const updateOperation = (operation) => {
-  //   Axios.put('http://localhost:3001/api/update', {
-  //     operationConcept: operation,
-  //     operationAmount: newAmount,
-  //   });
-  //   setNewAmount('');
-  // };
-
-  // const updateOperation = (operation) => {
-  //   console.log(operation);
-  // };
+  const updateOperation = (operation) => {
+    Axios.put('http://localhost:3001/api/update', {
+      operationId: operation,
+      operationConcept: newConcept,
+      operationAmount: newAmount,
+      operationDate: newDate,
+    });
+    setNewAmount('');
+  };
 
   return (
     <div className="App">
-      <h1>Nueva operación:</h1>
+      <p className="create-operation-title">Crear nueva operación:</p>
 
-      <div className="form">
-        <label htmlFor="operationConcept">
-          Concepto:
-          <input
-            type="text"
-            name="operationConcept"
-            onChange={(e) => {
-              setOperationConcept(e.target.value);
-            }}
-          />
-        </label>
-        <label htmlFor="operationAmount">
-          Monto:
-          <input
-            type="number"
-            name="operationAmount"
-            onChange={(e) => {
-              setOperationAmount(Number(e.target.value));
-            }}
-          />
-        </label>
-        <label htmlFor="operationDate">
-          Fecha:
-          <input
-            type="date"
-            name="operationDate"
-            onChange={(e) => {
-              setOperationDate(dateFormat(e.target.value, 'dd-mm-yyyy'));
-            }}
-          />
-        </label>
-        <label htmlFor="operationType">
-          Tipo:
-          <select
-            name="operationType"
-            onChange={(e) => {
-              setOperationType(e.target.value);
-            }}
-          >
-            <option value="in">Ingreso</option>
-            <option value="out">Egreso</option>
-          </select>
-        </label>
+      <CreateOperation
+        setOperationConcept={setOperationConcept}
+        setOperationAmount={setOperationAmount}
+        setOperationDate={setOperationDate}
+        setOperationType={setOperationType}
+        submitOperation={submitOperation}
+      />
 
-        <button type="button" onClick={submitOperation}>Submit</button>
-
-      </div>
+      {operationsList.map((val) => (
+        <List
+          key={val.id}
+          id={val.id}
+          concept={val.concept}
+          amount={val.amount}
+          date={val.date}
+          type={val.type === 'in' ? 'Ingreso' : 'Egreso'}
+          deleteOperation={() => {
+            if (window.confirm(`Estás seguro que quieres eliminar la operación ${val.concept}?`)) {
+              deleteOperation(val.id);
+              fetchData();
+            }
+          }}
+          updateOperation={() => updateOperation(val.id)}
+          editButtonHandler={(e) => {
+            e.target.parentElement.parentElement.childNodes[6].className = '';
+            setNewConcept(val.concept);
+            setNewAmount(val.amount);
+            setNewDate(val.date);
+          }}
+          handleNewConceptInput={setNewConcept}
+          handleNewAmountInput={setNewAmount}
+          handleNewDateInput={setNewDate}
+        />
+      ))}
 
       <br />
 
-      <div>
-        <table className="table table-bordered mb-5">
-          <thead>
-            <tr className="table-success">
-              <th scope="col">#</th>
-              <th scope="col">Concepto</th>
-              <th scope="col">Monto</th>
-              <th scope="col">Fecha</th>
-              <th scope="col">Tipo</th>
-              <th scope="col">Administrar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {operationsList.map((val) => (
-              <>
-                <List
-                  key={val.id}
-                  id={val.id}
-                  concept={val.concept}
-                  amount={val.amount}
-                  date={val.date}
-                  type={val.type === 'in' ? 'Ingreso' : 'Egreso'}
-                  updateClass=""
-                  deleteOperation={() => {
-                    deleteOperation(val.id);
-                  }}
-                  updateOperation={() => {
-                    console.log('ASD');
-                  }}
-                  editButtonHandler={(e) => {
-                    setOperationToUpdate(val);
-                    setUpdateClass('');
-                    console.log(e);
-                    console.log(operationToUpdate);
-                  }}
-                />
-                <UpdateList
-                  key={`${val.id}u`}
-                  id={val.id}
-                  concept={val.concept}
-                  amount={val.amount}
-                  date={val.date}
-                  type={val.type === 'in' ? 'Ingreso' : 'Egreso'}
-                  updateClass="hidden"
-                />
-              </>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {console.log(updateClass)}
     </div>
   );
 }
@@ -152,6 +87,7 @@ function Abm({ operationsList, setOperationsList }) {
 Abm.defaultProps = {
   operationsList: [],
   setOperationsList: () => {},
+  fetchData: () => {},
 };
 
 Abm.propTypes = {
@@ -163,6 +99,7 @@ Abm.propTypes = {
     type: PropTypes.string,
   })),
   setOperationsList: PropTypes.func,
+  fetchData: PropTypes.func,
 };
 
 export default Abm;
