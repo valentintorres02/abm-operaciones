219e2e674ce2d68071f09678600fecac666665ba
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-01-25 20:21:00 -0300

    Update Nabvar.js
    Update Nabvar to a more simple one.

--- client/src/Nabvar.js
+++ client/src/Nabvar.js
@@ -1,26 +1,15 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 
 function Nabvar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="/">
-              Home
-              <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="abm">ABM</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <ul className="nav">
+      <li className="nav-item">
+        <a className="nav-link active" href="/">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/abm">Abm</a>
+      </li>
+    </ul>
 
   );
 }
