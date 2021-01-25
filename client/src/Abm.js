3471eb3fda0459fb778cf0fb35abeabca5fabe42
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-01-25 20:17:47 -0300

    Update data tables and funcions
    New data tables to show the operations info and re-factored the update/delete operation funcionalities.

--- client/src/Abm.js
+++ client/src/Abm.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 import './App.css';
 import Axios from 'axios';
 import List from './List';
-import CreateOperation from './CreateOperation';
+import CreateOperationForm from './CreateOperationForm';
 
-function Abm({ fetchData, operationsList, setOperationsList }) {
+function Abm({ operationsList, setOperationsList }) {
   const [operationConcept, setOperationConcept] = useState('');
   const [operationAmount, setOperationAmount] = useState('');
   const [operationDate, setOperationDate] = useState('');
   const [operationType, setOperationType] = useState('in');
 
-  const [newConcept, setNewConcept] = useState('');
+  const [newConcept, setNewConcept] = useState('state');
   const [newAmount, setNewAmount] = useState(0);
   const [newDate, setNewDate] = useState('');
 
@@ -22,7 +22,7 @@ function Abm({ fetchData, operationsList, setOperationsList }) {
     setOperationsList([...operationsList, {
       operationConcept, operationAmount, operationDate, operationType,
     }]);
-    console.log('Operation submitted');
+    console.log(operationsList);
   };
 
   const deleteOperation = (operation) => {
@@ -43,7 +43,7 @@ function Abm({ fetchData, operationsList, setOperationsList }) {
     <div className="App">
       <p className="create-operation-title">Crear nueva operación:</p>
 
-      <CreateOperation
+      <CreateOperationForm
         setOperationConcept={setOperationConcept}
         setOperationAmount={setOperationAmount}
         setOperationDate={setOperationDate}
@@ -51,43 +51,92 @@ function Abm({ fetchData, operationsList, setOperationsList }) {
         submitOperation={submitOperation}
       />
 
-      {operationsList.map((val) => (
-        <List
-          key={val.id}
-          id={val.id}
-          concept={val.concept}
-          amount={val.amount}
-          date={val.date}
-          type={val.type === 'in' ? 'Ingreso' : 'Egreso'}
-          deleteOperation={() => {
-            if (window.confirm(`Estás seguro que quieres eliminar la operación ${val.concept}?`)) {
-              deleteOperation(val.id);
-              fetchData();
-            }
-          }}
-          updateOperation={() => updateOperation(val.id)}
-          editButtonHandler={(e) => {
-            e.target.parentElement.parentElement.childNodes[6].className = '';
-            setNewConcept(val.concept);
-            setNewAmount(val.amount);
-            setNewDate(val.date);
-          }}
-          handleNewConceptInput={setNewConcept}
-          handleNewAmountInput={setNewAmount}
-          handleNewDateInput={setNewDate}
-        />
-      ))}
+      <table className="table table-bordered mb-5">
+        <thead>
+          <tr className="table-success">
+            <th scope="col">#</th>
+            <th scope="col">Concepto</th>
+            <th scope="col">Monto</th>
+            <th scope="col">Fecha</th>
+            <th scope="col">Tipo</th>
+            <th scope="col">Administrar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {operationsList.map((val) => (
+            <List
+              key={val.id}
+              id={val.id}
+              concept={val.concept}
+              amount={Number(val.amount)}
+              date={val.date}
+              type={val.type === 'in' ? 'Ingreso' : 'Egreso'}
+              handleEditButton={(e) => {
+                e.target.parentElement.parentElement.childNodes[1].childNodes[0].readOnly = false;
+                e.target.parentElement.parentElement.childNodes[1].childNodes[0].className = '';
+                e.target.parentElement.parentElement.childNodes[2].childNodes[0].readOnly = false;
+                e.target.parentElement.parentElement.childNodes[2].childNodes[0].className = '';
+                e.target.parentElement.parentElement.childNodes[3].childNodes[0].readOnly = false;
+                e.target.parentElement.parentElement.childNodes[3].childNodes[0].className = '';
 
+                (e.target.parentElement.childNodes[1].className = 'hidden');
+                (e.target.parentElement.childNodes[2].className = 'hidden');
+                (e.target.parentElement.childNodes[0].className = 'btn btn-success');
+                setNewConcept(
+                  e.target.parentElement.parentElement.childNodes[1].childNodes[0].value,
+                );
+                setNewAmount(
+                  e.target.parentElement.parentElement.childNodes[2].childNodes[0].value,
+                );
+                setNewDate(
+                  e.target.parentElement.parentElement.childNodes[3].childNodes[0].value,
+                );
+              }}
+              handleDeleteButton={(e) => {
+                if (window.confirm(`Deseas eliminar la operación ${val.concept}?`)) {
+                  deleteOperation(val.id);
+                  e.target.parentElement.parentElement.innerHTML = '';
+                }
+              }}
+              setNewConcept={(e) => setNewConcept(e.target.value)}
+              setNewAmount={(e) => setNewAmount(Number(e.target.value))}
+              setNewDate={(e) => setNewDate(e.target.value)}
+              updateOperation={(e) => {
+                updateOperation(val.id);
+                e.target.parentElement.parentElement.childNodes[1].childNodes[0].readOnly = true;
+                e.target.parentElement.parentElement.childNodes[1].childNodes[0].className = 'input-table';
+                e.target.parentElement.parentElement.childNodes[2].childNodes[0].readOnly = true;
+                e.target.parentElement.parentElement.childNodes[2].childNodes[0].className = 'input-table';
+                e.target.parentElement.parentElement.childNodes[3].childNodes[0].readOnly = true;
+                e.target.parentElement.parentElement.childNodes[3].childNodes[0].className = 'input-table';
+
+                (e.target.parentElement.childNodes[1].className = 'btn btn-primary');
+                (e.target.parentElement.childNodes[2].className = 'btn btn-danger');
+                (e.target.parentElement.childNodes[0].className = 'hidden');
+              }}
+              editButtonHandler={(e) => {
+                e.target.parentElement.parentElement.childNodes[6].className = '';
+                setNewConcept(val.concept);
+                setNewAmount(val.amount);
+                setNewDate(val.date);
+              }}
+              handleNewConceptInput={setNewConcept}
+              handleNewAmountInput={setNewAmount}
+              handleNewDateInput={setNewDate}
+            />
+          ))}
+        </tbody>
+      </table>
       <br />
 
     </div>
+
   );
 }
 
 Abm.defaultProps = {
   operationsList: [],
   setOperationsList: () => {},
-  fetchData: () => {},
 };
 
 Abm.propTypes = {
@@ -99,7 +148,6 @@ Abm.propTypes = {
     type: PropTypes.string,
   })),
   setOperationsList: PropTypes.func,
-  fetchData: PropTypes.func,
 };
 
 export default Abm;
