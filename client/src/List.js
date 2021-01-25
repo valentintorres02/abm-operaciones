1c023280d04f19584cfe3d966fd7f62c82472c27
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-01-25 20:21:31 -0300

    Update List.js
    Update List and deleted some code that wasn't necessary due to refactor code in Abm.js

--- client/src/List.js
+++ client/src/List.js
@@ -3,36 +3,29 @@ import './App.css';
 import PropTypes from 'prop-types';
 
 function List({
-  id, concept, amount, date, type, deleteOperation, updateClass, updateOperation,
+  id, concept, amount, date, type,
+  handleEditButton, inputClass, setNewConcept, setNewAmount, setNewDate,
+  updateOperation, handleDeleteButton,
 }) {
   return (
     <>
       <tr>
         <th scope="row">{id}</th>
         <td>
-          {concept}
-          {' '}
-          <input className="hidden" />
+          <input className={inputClass} defaultValue={concept} readOnly="readonly" onChange={setNewConcept} />
         </td>
-        <td>{amount}</td>
-        <td>{date}</td>
-        <td>{type}</td>
         <td>
-          <button type="button" className="btn btn-primary btn-sm">Editar</button>
-          <button type="button" className="btn btn-danger btn-sm" onClick={deleteOperation}>Eliminar</button>
+          <input className={inputClass} defaultValue={amount} readOnly="readonly" onChange={setNewAmount} />
+        </td>
+        <td><input className={inputClass} defaultValue={date} readOnly="readonly" onChange={setNewDate} /></td>
+        <td><input className={inputClass} defaultValue={type} disabled /></td>
+        <td>
+          <button type="button" className="hidden" onClick={updateOperation}>Confirmar</button>
+          <button type="button" className="btn btn-primary" onClick={handleEditButton}>Editar</button>
+          <button type="button" className="btn btn-danger" onClick={handleDeleteButton}>Eliminar</button>
         </td>
       </tr>
-      <tr className={updateClass}>
-        <th scope="row">{id}</th>
-        <td><input className="form-control input-sm" /></td>
-        <td><input className="form-control input-sm" /></td>
-        <td><input className="form-control input-sm" /></td>
-        <td><input className="form-control input-sm" /></td>
-        <td>
-          <button type="button" className="btn btn-primary btn-sm" onClick={updateOperation}>Editar</button>
-          <button type="button" className="btn btn-danger btn-sm" onClick={deleteOperation}>Eliminar</button>
-        </td>
-      </tr>
+
     </>
   );
 }
@@ -43,20 +36,28 @@ List.defaultProps = {
   amount: 0,
   date: '',
   type: '',
-  updateClass: 'hidden',
-  deleteOperation: () => {},
+  inputClass: 'input-table',
+  handleEditButton: () => {},
+  setNewConcept: () => {},
+  setNewAmount: () => {},
+  setNewDate: () => {},
   updateOperation: () => {},
+  handleDeleteButton: () => {},
 };
 
 List.propTypes = {
   id: PropTypes.number,
   concept: PropTypes.string,
-  amount: PropTypes.string,
+  amount: PropTypes.number,
   date: PropTypes.string,
   type: PropTypes.string,
-  updateClass: PropTypes.string,
-  deleteOperation: PropTypes.func,
+  inputClass: PropTypes.string,
+  handleEditButton: PropTypes.func,
+  setNewConcept: PropTypes.func,
+  setNewAmount: PropTypes.func,
+  setNewDate: PropTypes.func,
   updateOperation: PropTypes.func,
+  handleDeleteButton: PropTypes.func,
 };
 
 export default List;
