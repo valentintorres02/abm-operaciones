aed79370d81376b7353417e5a68e6ea58ac1def7
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-01-26 15:24:00 -0300

    Change database file
    Change database file|

--- databases/abm-operations.sql
+++ databases/abm-operations.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: abm_operaciones
+-- Host: 127.0.0.1    Database: abm_operations
 -- ------------------------------------------------------
 -- Server version	8.0.22
 
@@ -25,11 +25,11 @@ DROP TABLE IF EXISTS `operations`;
 CREATE TABLE `operations` (
   `id` int NOT NULL AUTO_INCREMENT,
   `concept` varchar(200) NOT NULL,
-  `amount` varchar(50) NOT NULL,
-  `date` varchar(100) DEFAULT NULL,
-  `type` varchar(45) DEFAULT NULL,
+  `amount` varchar(45) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `type` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=197 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,9 +38,13 @@ CREATE TABLE `operations` (
 
 LOCK TABLES `operations` WRITE;
 /*!40000 ALTER TABLE `operations` DISABLE KEYS */;
-INSERT INTO `operations` VALUES (181,'Ventas','13000','04-01-2021','in'),(184,'Compra de mercadería','10000','02-01-2021','out'),(185,'Compra','15000','30-12-2020','out'),(196,'123','456','30-12-2021','in');
+INSERT INTO `operations` VALUES (1,'Venta de insumos','7500','09-10-2020','in'),(2,'Compra de mercadería','9000','21-10-2020','out'),(3,'Pago de impuestos','2000','29-10-2020','in'),(4,'Cheques cobrados','12000','02-12-2020','in'),(5,'Compra de útiles','100','10-12-2020','out'),(6,'Pago de deudas','25000','07-01-2021','out'),(7,'Ventas','18000','27-12-2020','in'),(8,'Pago de comisiones','1700','08-01-2021','out'),(9,'Compra de comida','375','28-12-2020','out'),(10,'Compra de mercadería','7000','14-12-2020','out'),(11,'Compra de activos','4000','28-11-2020','out'),(12,'Venta de inmueble','18000','15-02-2020','in'),(13,'Pago de servicios','2500','11-11-2020','out'),(14,'Cobro de bonos','1200','22-01-2021','in'),(15,'Pago a proveedores','4000','01-01-2021','out'),(16,'Compras','1200','01-01-2021','out'),(17,'Cobro de deudas','1000','20-10-2020','in');
 /*!40000 ALTER TABLE `operations` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'abm_operations'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -51,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-26 12:57:01
+-- Dump completed on 2021-01-26 14:58:50
