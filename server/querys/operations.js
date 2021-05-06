8cdbfe35c85205d2d31d905e3ca4aa6ece1e25cc
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-06 15:15:41 -0300

    Fix: Rename querys functions

--- server/querys/operations.js
+++ server/querys/operations.js
@@ -1,7 +1,7 @@
 const db = require('../models');
 const Operation = db.Operation;
 
-exports.createOperation = (concept, amount, date, type) => {
+exports.queryCreateOperation = (concept, amount, date, type) => {
   return Operation.create({
     concept,
     amount,
@@ -10,12 +10,12 @@ exports.createOperation = (concept, amount, date, type) => {
   });
 };
 
-exports.getAllOperations = async () => {
+exports.queryGetAllOperations = async () => {
   const operations = await Operation.findAll();
   return operations;
 };
 
-exports.getAllOperationsByType = async (type) => {
+exports.queryGetAllOperationsByType = async (type) => {
   let operationsByType = await Operation.findAll({
     where: {
       type: type,
