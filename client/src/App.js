b16787cbfac70cafd109cca47869e3cc0520f41e
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-07 17:18:00 -0300

    Refactor: Delete unused file, and its imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import { Route } from 'react-router-dom';
 import HomeScreen from './screens/Home/HomeScreen';
-import Abm from './components/Abm';
 import NabvarComponent from './components/Nabvar/NabvarComponent';
 import CreateOperationScreen from './screens/CreateOperation/CreateOperationScreen';
 
 function App() {
-  const [operationsList, setOperationsList] = useState([]);
-
-  const fetchData = () => {
-    Axios.get('http://localhost:3000/operation')
-      .then((response) => {
-        setOperationsList(response.data);
-        return response.data;
-      });
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <div>
       <NabvarComponent />
@@ -29,16 +14,6 @@ function App() {
         path="/"
         render={() => (
           <HomeScreen />
-        )}
-      />
-      <Route
-        exact
-        path="/abm"
-        render={() => (
-          <Abm
-            operationsList={operationsList}
-            setOperationsList={setOperationsList}
-          />
         )}
       />
       <Route
