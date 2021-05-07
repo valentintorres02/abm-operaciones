91514b13b2ab7a4ef7c5d7f5d328740c3c9b8809
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-07 17:25:38 -0300

    Refactor: Modularize App imports into routes file

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import React from 'react';
 import { Route } from 'react-router-dom';
-import HomeScreen from './screens/Home/HomeScreen';
 import NabvarComponent from './components/Nabvar/NabvarComponent';
-import CreateOperationScreen from './screens/CreateOperation/CreateOperationScreen';
+import routes from './routes/routes';
 
 function App() {
   return (
     <div>
       <NabvarComponent />
-      <Route
-        exact
-        path="/"
-        render={() => (
-          <HomeScreen />
-        )}
-      />
-      <Route
-        exact
-        path="/create-operation"
-        render={() => (
-          <CreateOperationScreen />
-        )}
-      />
+      {routes.map(({ path, Component }) => (
+        <Route key={path} exact path={path}>
+          <Component />
+        </Route>
+      ))}
     </div>
   );
 }
---
+++ client/src/routes/routes.js
@@ -0,0 +1,9 @@
+import HomeScreen from "../screens/Home/HomeScreen";
+import OperationsScreen from "../screens/Operations/OperationsScreen";
+
+const routes = [
+  { path: "/", Component: HomeScreen },
+  { path: "/operations", Component: OperationsScreen },
+];
+
+export default routes;
