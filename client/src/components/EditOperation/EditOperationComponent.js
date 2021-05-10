2d489cf48886d6b9922521f474ca5b5931a14760
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-10 13:44:32 -0300

    Feat: Create basic EditOp comp, screen and route

---
+++ client/src/components/EditOperation/EditOperationComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditOperationComponent() {
+  return (
+    <div>
+      EditOperation
+    </div>
+  );
+};
+
+export default EditOperationComponent;
---
+++ client/src/screens/EditOperation/EditOperationScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditOperationComponent from '../../components/EditOperation/EditOperationComponent';
+
+function EditOperationScreen() {
+  return (
+    <div>
+      <EditOperationComponent />
+    </div>
+  );
+};
+
+export default EditOperationScreen;
