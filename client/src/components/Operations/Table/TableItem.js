4c197f47e95c50ccee214f6bcbfc532c53ff3150
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-11 17:16:48 -0300

    Feature: Add onClick to delete button

--- client/src/components/Operations/Table/TableItem.js
+++ client/src/components/Operations/Table/TableItem.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../../../App.css';
 import { Link } from 'react-router-dom';
+import { deleteOperation, OPERATION_URL_MAIN } from '../../../constants/constants';
+import { httpDelete } from '../../../services/httpServices';
 
 function TableItem({
-  id, concept, amount, date, type, administrableOperations
+  id, concept, amount, date, type, administrableOperations, state, setState
 }) {
   return (
     <>
@@ -18,7 +20,9 @@ function TableItem({
             <Link to={`/edit-operation/${id}`}>
               <button type="button" className="btn btn-primary">Editar</button>
             </Link>
-            <button type="button" className="btn btn-danger">Eliminar</button>
+            <button type="button" className="btn btn-danger" id={id} onClick={(e) => {
+              deleteOperation(e, httpDelete, OPERATION_URL_MAIN, state, setState)
+            }}>Eliminar</button>
           </td>
           : null}
       </tr>
