d87269e795fdb290b5b4670e2662c7d688e7c4a4
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-11 18:42:46 -0300

    Refactor: Modularize CreateOperation component

--- client/src/components/CreateOperation/CreateOperationComponent.js
+++ client/src/components/CreateOperation/CreateOperationComponent.js
@@ -1,22 +1,14 @@
 import React, { useState } from 'react';
-import { CREATE_OPERATION_TITLE, OPERATION_URL_MAIN, CREATE_FORM_BUTTON_TITLE, createOperation } from '../../constants/constants';
-import { httpPost } from '../../services/httpServices';
-import OperationForm from '../OperationForm/OperationForm';
+import CreateOperationContent from './CreateOperationContent';
 
 function CreateOperationComponent() {
   const [newOperation, setNewOperation] = useState({ type: 'in' });
 
   return (
-    <div>
-      <OperationForm
-        title={CREATE_OPERATION_TITLE}
-        handleSubmit={(e) => createOperation(e, httpPost, OPERATION_URL_MAIN, newOperation)}
-        setManagedOperation={setNewOperation}
-        managedOperation={newOperation}
-        operationToManage={newOperation}
-        formButtonTitle={CREATE_FORM_BUTTON_TITLE}
-      />
-    </div>
+    <CreateOperationContent
+      newOperation={newOperation}
+      setNewOperation={setNewOperation}
+    />
   );
 };
 
