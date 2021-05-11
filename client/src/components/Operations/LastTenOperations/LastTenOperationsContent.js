09b8d90f6cd9d6e39106872427d648fbc9e408d7
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-11 18:56:04 -0300

    Refactor: Replace title w/ common Operationstitle

--- client/src/components/Operations/LastTenOperations/LastTenOperationsContent.js
+++ client/src/components/Operations/LastTenOperations/LastTenOperationsContent.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { LAST_TEN_OPERATIONS_TITLE } from '../../../constants/constants';
+import OperationsTitle from '../OperationsTitle';
 import Table from '../Table/Table';
-import LastTenOperationsTitle from './LastTenOperationsTitle';
 
 function LastTenOperationsContent({ operationsList }) {
   return (
     <div>
-      <LastTenOperationsTitle />
+      <OperationsTitle titleContent={LAST_TEN_OPERATIONS_TITLE} />
       <Table operations={operationsList} administrableOperations={false} />
     </div>
   );
