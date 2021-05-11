7688389646d34664c2241c17083842bc5a53b923
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-05-11 17:20:57 -0300

    Feature: Add create operation to nabvar and route

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,11 +1,13 @@
 import EditOperationScreen from "../screens/EditOperation/EditOperationScreen";
 import HomeScreen from "../screens/Home/HomeScreen";
 import OperationsScreen from "../screens/Operations/OperationsScreen";
+import CreateOperationScreen from "../screens/CreateOperation/CreateOperationScreen";
 
 const routes = [
   { path: "/", Component: HomeScreen },
   { path: "/operations", Component: OperationsScreen },
   { path: "/edit-operation/:id", Component: EditOperationScreen },
+  { path: "/create-operation", Component: CreateOperationScreen },
 ];
 
 export default routes;
