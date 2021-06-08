d2e944ddb5eb0ea5574cd98429a98c05a4608d91
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-06-08 10:26:27 -0300

    Feature: Get DB cfg from env variables

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: "localhost",
-    user: "root",
-    password: "Password123",
-    database: "crud-operations"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 }
