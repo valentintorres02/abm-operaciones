8535e9b236624f0589e4982019d50d913370e798
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-06-08 10:40:38 -0300

    Feature: Add repository link to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import NabvarComponent from './components/Nabvar/NabvarComponent';
+import Repository from './components/Repository/Repository';
 import routes from './routes/routes';
 
 function App() {
   return (
     <div>
+      <Repository />
       <NabvarComponent />
       {routes.map(({ path, Component }) => (
         <Route key={path} exact path={path}>
           <Component />
         </Route>
       ))}
+
     </div>
   );
 }
---
+++ client/src/components/Repository/Repository.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Repository = () => {
+  return (
+    <div class="alert alert-primary" role="alert">
+      Link del repositorio click <a href="https://github.com/valentintorres02/crud-operations">aquí</a>
+    </div>
+  )
+}
+
+export default Repository
